cde6cc7f1e4d080150a2c27024bebf89c20b1f04
WinterFellCore <bobruapela@gmail.com>
2026-05-02 10:32:17 -0300

    fix: corrigir CORS headers

--- api/check-web-account.js
+++ api/check-web-account.js
@@ -17,6 +17,7 @@ function json(res, code, obj) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Max-Age', '86400');
   res.end(JSON.stringify(obj));
 }
 
